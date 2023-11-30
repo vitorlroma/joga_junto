7a8c43263a7b74b6af5158387417b623244768a6
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-11-29 23:19:15 -0300

    sign in and sign up without google sign in implemented

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:joga_junto/core/utils.dart';
@@ -13,6 +14,38 @@ class AuthController {
 
   void signInWithGoogle(BuildContext context) async {
     final user = await _authRepository.signInWithGoogle();
+    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
+
+  void signIn(BuildContext context, String email, String password) async {
+    final user = await _authRepository.signInUser(email: email, password: password);
+    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
+
+  void signUp({
+      required BuildContext context,
+      required String profilePic,
+      required String name,
+      required String email,
+      required String password,
+      required String cpf,
+      required List<String> address,
+      required String cityState,
+      required String statistics,
+      required int karma
+    }) async {
+    final user = await _authRepository.signUpUser(
+      profilePic: profilePic, 
+      name: name, 
+      email: email, 
+      password: password, 
+      cpf: cpf, 
+      address: address, 
+      cityState: cityState, 
+      statistics: statistics, 
+      karma: karma, 
+      file: Uint8List(0),
+      );
     user.fold((l) => showSnackBar(context, l.message), (r) => null);
   }
 }
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -47,12 +48,16 @@ class AuthRepository {
 
         if(userCredential.additionalUserInfo!.isNewUser){
           userModel = UserModel(
-            name: userCredential.user!.displayName??'No name',
-            profilePic: userCredential.user!.photoURL??Constants.avatarDefault,
-            banner: Constants.bannerDefault,
             uid: userCredential.user!.uid,
-            isAuthentication: true, karma: 0,
-            awards: []
+            profilePic: userCredential.user!.photoURL??Constants.avatarDefault,
+            name: userCredential.user!.displayName??'No name',
+            email: userCredential.user!.email??'',
+            password: credential.accessToken??'',
+            cpf: '',
+            address: [],
+            cityState: '',
+            statistics: '',
+            karma: 0,
           );
           await _users.doc(userCredential.user!.uid).set(userModel.toMap());
         }
@@ -63,4 +68,81 @@ class AuthRepository {
         return left(Failure(e.toString()));
       }
     }
+
+    FutureEither<UserModel> signUpUser({
+      required String profilePic,
+      required String name,
+      required String email,
+      required String password,
+      required String cpf,
+      required List<String> address,
+      required String cityState,
+      required String statistics,
+      required int karma,
+      required Uint8List file,
+    }) async {
+      try {
+        late UserModel userModel;
+        
+        if (email.isNotEmpty ||
+            password.isNotEmpty ||
+            name.isNotEmpty ||
+            cpf.isEmpty ||
+            address.isEmpty ||
+            cityState.isEmpty ||
+            statistics.isEmpty ||
+            karma.isNaN
+            ) {
+          UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+
+          userModel = UserModel(
+            uid: userCredential.user!.uid, 
+            profilePic: profilePic,
+            name: name,
+            email: userCredential.user!.email??'',
+            password: password,
+            cpf: cpf,
+            address: [],
+            cityState: cityState,
+            statistics: statistics,
+            karma: 0,
+          );
+          await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+        }
+        return right(userModel);
+      } on FirebaseException catch(e){
+        throw e.message!;
+      } catch (e){
+        return left(Failure(e.toString()));
+      }
+    }
+
+    FutureEither<UserModel> signInUser({
+      required String email,
+      required String password,
+    }) async {
+      try{
+        late UserModel userModel;
+
+        if (email.isNotEmpty || password.isNotEmpty) {
+          final credential = await _auth.signInWithEmailAndPassword(
+            email: email, 
+            password: password
+          );
+          userModel = credential.user as UserModel;
+        }
+        return right(userModel);
+      } on FirebaseException catch(e){
+        throw e.message!;
+      } catch (e){
+        return left(Failure(e.toString()));
+      }
+    }
+
+    Future<void> signOut() async {
+    await _auth.signOut();
+  }
 }
