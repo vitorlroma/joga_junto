ee15125291141217a22e682de763b2d9ffb7bf15
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-11-29 23:18:05 -0300

    rewrote user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,98 +1,124 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:collection/collection.dart';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
 
 class UserModel {
-  final String name;
-  final String profilePic;
-  final String banner;
   final String uid;
-  final bool isAuthentication;
+  final String profilePic;
+  final String name;
+  final String email;
+  final String password;
+  final String cpf;
+  final List<String> address;
+  final String cityState;
+  final String statistics;
   final int karma;
-  final List<String> awards;
-  
   UserModel({
-    required this.name,
-    required this.profilePic,
-    required this.banner,
     required this.uid,
-    required this.isAuthentication,
+    required this.profilePic,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.cpf,
+    required this.address,
+    required this.cityState,
+    required this.statistics,
     required this.karma,
-    required this.awards,
   });
-  
 
   UserModel copyWith({
-    String? name,
-    String? profilePic,
-    String? banner,
     String? uid,
-    bool? isAuthentication,
+    String? profilePic,
+    String? name,
+    String? email,
+    String? password,
+    String? cpf,
+    List<String>? address,
+    String? cityState,
+    String? statistics,
     int? karma,
-    List<String>? awards,
   }) {
     return UserModel(
-      name: name ?? this.name,
-      profilePic: profilePic ?? this.profilePic,
-      banner: banner ?? this.banner,
       uid: uid ?? this.uid,
-      isAuthentication: isAuthentication ?? this.isAuthentication,
+      profilePic: profilePic ?? this.profilePic,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      cpf: cpf ?? this.cpf,
+      address: address ?? this.address,
+      cityState: cityState ?? this.cityState,
+      statistics: statistics ?? this.statistics,
       karma: karma ?? this.karma,
-      awards: awards ?? this.awards,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
-      'name': name,
-      'profilePic': profilePic,
-      'banner': banner,
       'uid': uid,
-      'isAuthentication': isAuthentication,
+      'profilePic': profilePic,
+      'name': name,
+      'email': email,
+      'password': password,
+      'cpf': cpf,
+      'addres': address,
+      'cityState': cityState,
+      'statistics': statistics,
       'karma': karma,
-      'awards': awards,
     };
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
-      name: map['name'] as String,
-      profilePic: map['profilePic'] as String,
-      banner: map['banner'] as String,
       uid: map['uid'] as String,
-      isAuthentication: map['isAuthentication'] as bool,
+      profilePic: map['profilePic'] as String,
+      name: map['name'] as String,
+      email: map['email'] as String,
+      password: map['password'] as String,
+      cpf: map['cpf'] as String,
+      address: List<String>.from((map['address'] as List<String>)),
+      cityState: map['cityState'] as String,
+      statistics: map['statistics'] as String,
       karma: map['karma'] as int,
-      awards: List<String>.from((map['awards'] as List<String>))
     );
   }
 
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
   @override
   String toString() {
-    return 'UserModel(name: $name, profilePic: $profilePic, banner: $banner, uid: $uid, isAuthentication: $isAuthentication, karma: $karma, awards: $awards)';
+    return 'UserModel(uid: $uid, profilePic: $profilePic, name: $name, email: $email, password: $password, cpf: $cpf, address: $address, cityState: $cityState, statistics: $statistics, karma: $karma)';
   }
 
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
-    final listEquals = const DeepCollectionEquality().equals;
   
     return 
-      other.name == name &&
-      other.profilePic == profilePic &&
-      other.banner == banner &&
       other.uid == uid &&
-      other.isAuthentication == isAuthentication &&
-      other.karma == karma &&
-      listEquals(other.awards, awards);
+      other.profilePic == profilePic &&
+      other.name == name &&
+      other.email == email &&
+      other.password == password &&
+      other.cpf == cpf &&
+      listEquals(other.address, address) &&
+      other.cityState == cityState &&
+      other.statistics == statistics &&
+      other.karma == karma;
   }
 
   @override
   int get hashCode {
-    return name.hashCode ^
+    return uid.hashCode ^
       profilePic.hashCode ^
-      banner.hashCode ^
-      uid.hashCode ^
-      isAuthentication.hashCode ^
-      karma.hashCode ^
-      awards.hashCode;
+      name.hashCode ^
+      email.hashCode ^
+      password.hashCode ^
+      cpf.hashCode ^
+      address.hashCode ^
+      cityState.hashCode ^
+      statistics.hashCode ^
+      karma.hashCode;
   }
 }
