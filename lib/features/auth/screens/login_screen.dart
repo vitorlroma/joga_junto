bc977ff1fdc3fa888074cbbe975222bde07da0e3
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-11-29 23:19:51 -0300

    rewrote screen using form

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:joga_junto/core/common/signin_google_button.dart';
 import 'package:joga_junto/theme/pallete.dart';
 import 'package:loading_overlay/loading_overlay.dart';
-import 'package:joga_junto/core/common/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,93 +12,115 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late String _email;
-  late String _password;
+  final formKey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final senha = TextEditingController();
+  // ignore: prefer_final_fields
   bool _saving = false;
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        Navigator.popAndPushNamed(context, HomeScreen.id);
-        return false;
-      },
-      child: Scaffold(
-        body: LoadingOverlay(
-          isLoading: _saving,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Pallete.orangeColor,),
+      body: LoadingOverlay(
+        isLoading: _saving,
+        child: SafeArea(
+          child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+            child: Form(
+              key: formKey,
               child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [const Text('Cadastro',
-                        style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          border: Border.all(
-                            width: 2.5,
-                            color: Pallete.orangeColor,
-                          ),
-                        ),
-                        child: TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: const InputDecoration(
-                            hintText: 'E-mail'
-                          ),
-                          onChanged: (value) {
-                            _email = value;
-                          },
-                          style: const TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 30,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          border: Border.all(
-                            width: 2.5,
-                            color: Pallete.orangeColor,
-                          ),
-                        ),
-                        child: TextField(
-                          decoration: const InputDecoration(
-                            hintText: 'Senha'
-                          ),
-                          obscureText: true,
-                          onChanged: (value) {
-                            _password = value;
-                          },
-                          style: const TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      const SignInGoogle(),
-                    ],
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [ 
+                  const Text(
+                    'titulo',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: -1.5,
                     ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      controller: email,
+                      decoration:const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Informe o email corretamente!';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                    child: TextFormField(
+                      controller: senha,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Senha',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Informa sua senha!';
+                        } else if (value.length < 6) {
+                          return 'Sua senha deve ter no mínimo 6 caracteres';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+
+                        }
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: (_saving)
+                            ? [
+                                const Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                )
+                              ]
+                            : [
+                                const Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Text(
+                                    'actionButton',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                              ],
+                      ),
+                    ),
+                  ),
+                  const SignInGoogle(),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('toggleButton'),
                   ),
                 ],
               ),
             ),
           ),
-        ),
+        )
       ),
     );
   }
