6b1e4f22b830d198569add689327806c3db8c18c
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-04 23:34:53 -0300

    added providers and streams to manage user state

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,25 +1,54 @@
 import 'dart:typed_data';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:joga_junto/core/utils.dart';
 import 'package:joga_junto/features/auth/repository/auth_repository.dart';
+import 'package:joga_junto/models/user_model.dart';
 
-final authControllerProvider = Provider((ref) => AuthController(authRepository: ref.read(authRepositoryProvider)));
 
-class AuthController {
+final userProvider = StateProvider<UserModel?>((ref) => null);
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
+  (ref) => AuthController(
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
+  )
+);
+
+final authStateChangeProvider = StreamProvider((ref) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.authStateChange;
+});
+
+final getUserDataProvider = StreamProvider.family((ref, String uid) {
+  final authController = ref.watch(authControllerProvider.notifier);
+  return authController.getUserData(uid);
+});
+
+class AuthController extends StateNotifier<bool>{
   final AuthRepository _authRepository;
+  final Ref _ref;
   AuthController({
-    required AuthRepository authRepository
-    }): _authRepository = authRepository;
+    required AuthRepository authRepository,
+    required Ref ref,
+    }): _authRepository = authRepository, 
+      _ref = ref,
+      super(false);
+
+  Stream<User?> get authStateChange => _authRepository.authStateChange; 
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
+    state = false;
     user.fold((l) => showSnackBar(context, l.message), (r) => null);
   }
 
   void signIn(BuildContext context, String email, String password) async {
+    state = true;
     final user = await _authRepository.signInUser(email: email, password: password);
-    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+    state = false;
+    user.fold((l) => showSnackBar(context, l.message), (userModel) => _ref.read(userProvider.notifier).update((state) => userModel));
   }
 
   void signUp(
@@ -30,22 +59,22 @@ class AuthController {
       String password,
       String cpf,
       List<String> address,
-      String cityState,
-      String statistics,
-      int karma
     ) async {
+      state = true;
     final user = await _authRepository.signUpUser(
       profilePic: profilePic, 
       name: name, 
       email: email, 
       password: password, 
       cpf: cpf, 
-      address: address, 
-      cityState: cityState, 
-      statistics: statistics, 
-      karma: karma, 
+      address: address,
       file: Uint8List(0),
       );
+      state = false;
     user.fold((l) => showSnackBar(context, l.message), (r) => null);
+  }
+
+  Stream<UserModel> getUserData(String uid){
+    return _authRepository.getUserData(uid);
   }
 }
--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -32,6 +32,8 @@ class AuthRepository {
 
     CollectionReference get _users => _firestore.collection(FirebaseConstants.usersCollection);
 
+    Stream<User?> get authStateChange => _auth.authStateChanges();
+
     FutureEither<UserModel> signInWithGoogle() async{
       try{
         final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -44,7 +46,7 @@ class AuthRepository {
 
         UserCredential userCredential = await _auth.signInWithCredential(credential);
         
-        late UserModel userModel;
+        UserModel userModel;
 
         if(userCredential.additionalUserInfo!.isNewUser){
           userModel = UserModel(
@@ -60,57 +62,8 @@ class AuthRepository {
             karma: 0,
           );
           await _users.doc(userCredential.user!.uid).set(userModel.toMap());
-        }
-        return right(userModel);
-      } on FirebaseException catch(e){
-        throw e.message!;
-      } catch (e){
-        return left(Failure(e.toString()));
-      }
-    }
-
-    FutureEither<UserModel> signUpUser({
-      required String profilePic,
-      required String name,
-      required String email,
-      required String password,
-      required String cpf,
-      required List<String> address,
-      required String cityState,
-      required String statistics,
-      required int karma,
-      required Uint8List file,
-    }) async {
-      try {
-        late UserModel userModel;
-        
-        if (email.isNotEmpty ||
-            password.isNotEmpty ||
-            name.isNotEmpty ||
-            cpf.isEmpty ||
-            address.isEmpty ||
-            cityState.isEmpty ||
-            statistics.isEmpty ||
-            karma.isNaN
-            ) {
-          UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-            email: email,
-            password: password,
-          );
-
-          userModel = UserModel(
-            uid: userCredential.user!.uid, 
-            profilePic: profilePic,
-            name: name,
-            email: userCredential.user!.email??'',
-            password: password,
-            cpf: cpf,
-            address: [],
-            cityState: cityState,
-            statistics: statistics,
-            karma: 0,
-          );
-          await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+        } else {
+          userModel = await getUserData(userCredential.user!.uid).first;
         }
         return right(userModel);
       } on FirebaseException catch(e){
@@ -128,11 +81,11 @@ class AuthRepository {
         late UserModel userModel;
 
         if (email.isNotEmpty || password.isNotEmpty) {
-          final credential = await _auth.signInWithEmailAndPassword(
+          final userCredential = await _auth.signInWithEmailAndPassword(
             email: email, 
             password: password
           );
-          userModel = credential.user as UserModel;
+          userModel = await getUserData(userCredential.user!.uid).first;
         }
         return right(userModel);
       } on FirebaseException catch(e){
@@ -140,6 +93,55 @@ class AuthRepository {
       } catch (e){
         return left(Failure(e.toString()));
       }
+    }
+
+    FutureEither<UserModel> signUpUser({
+      required String profilePic,
+      required String name,
+      required String email,
+      required String password,
+      required String cpf,
+      required List<String> address,
+      required Uint8List file,
+    }) async {
+      try {
+        late UserModel userModel;
+        
+        if (email.isNotEmpty ||
+            password.isNotEmpty ||
+            name.isNotEmpty ||
+            cpf.isEmpty ||
+            address.isEmpty
+            ) {
+          UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+
+          userModel = UserModel(
+            uid: userCredential.user!.uid, 
+            profilePic: profilePic,
+            name: name,
+            email: userCredential.user!.email??'',
+            password: password,
+            cpf: cpf,
+            address: [],
+            cityState: '',
+            statistics: '',
+            karma: 0,
+          );
+          await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+        }
+        return right(userModel);
+      } on FirebaseException catch(e){
+        throw e.message!;
+      } catch (e){
+        return left(Failure(e.toString()));
+      }
+    }
+
+    Stream<UserModel> getUserData(String uid){
+      return _users.doc(uid).snapshots().map((event) => UserModel.fromMap(event.data() as Map<String, dynamic>));
     }
 
     Future<void> signOut() async {
