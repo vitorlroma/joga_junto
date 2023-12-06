ecae3dc82abfe228a2ee07d3c64fca19a25282d0
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-05 23:30:21 -0300

    added sign-up and create-team routes

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:joga_junto/features/auth/screens/login_screen.dart';
+import 'package:joga_junto/features/auth/screens/signup_screen.dart';
 import 'package:joga_junto/features/home/screen/home_screen.dart';
+import 'package:joga_junto/features/team/screens/create_team_screen.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: LoginScreen())
+  '/': (_) => const MaterialPage(child: LoginScreen()),
+  '/sign-up': (_) => const MaterialPage(child: SignUpScreen()),
 });
 
 final loggedInRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen())
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-team': (_) => const MaterialPage(child: CreateTeamScreen()),
 });
