e300c991fd75c2761736fbf5d92f8d809eb1c983
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-05 23:14:14 -0300

    dispose() added, navigation with Routemaster setup, widgets reorganized

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:joga_junto/core/common/common_style.dart';
 import 'package:joga_junto/core/common/loader.dart';
 import 'package:joga_junto/core/constants/constants.dart';
 import 'package:joga_junto/features/auth/controller/auth_controller.dart';
-import 'package:joga_junto/features/auth/screens/signup_screen.dart';
 import 'package:joga_junto/theme/pallete.dart';
+import 'package:routemaster/routemaster.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
-  static String id = 'login_screen';
 
   @override
   ConsumerState<LoginScreen> createState() => _LoginScreenState();
@@ -20,12 +20,23 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
   final _email = TextEditingController();
   final _password = TextEditingController();
 
+  @override
+  void dispose() {
+    super.dispose();
+    _email.dispose();
+    _password.dispose();
+  }
+
   void signInWithEmailAndPassword(BuildContext context){
     ref.read(authControllerProvider.notifier).signIn(context, _email.text, _password.text);
   }
 
   void signInWithGoogle(BuildContext context, WidgetRef ref) {
     ref.read(authControllerProvider.notifier).signInWithGoogle(context);
+  }
+
+  void navigateToSignUpScreen(BuildContext context) {
+    Routemaster.of(context).push('/sign-up');
   }
 
   @override
@@ -37,124 +48,111 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
         ? const Loader() 
         : SingleChildScrollView(
         child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 25),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [ 
-                const Text(
-                  'JogaJunto',
-                  style: TextStyle(
-                    color: Pallete.orangeColor,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.5,
-                  ),
+        padding: bodyPading,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [ 
+              const Text(
+                'JogaJunto',
+                style: TextStyle(
+                  color: Pallete.orangeColor,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: -1.5,
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 24),
-                  child: TextFormField(
-                    controller: _email,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: const BorderSide(),
-                      ),
-                      labelText: 'E-mail',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe o e-mail corretamente!';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 24),
-                  child: TextFormField(
-                    controller: _password,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: const BorderSide(),
-                      ),
-                      labelText: 'Senha',
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informa sua senha!';
-                      } else if (value.length < 6) {
-                        return 'Sua senha deve ter no mínimo 6 caracteres';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        signInWithEmailAndPassword(context);
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Pallete.orangeColor,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)
-                      )
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                      child: Text(
-                        'Login',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),  
-                  child: ElevatedButton.icon(
-                    onPressed: () => signInWithGoogle(context, ref),
-                    icon: Flexible(
-                      child: Image.asset(
-                        Constants.googleLogoPath,
-                        width: 10,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: formFieldPadding,
+                      child: TextFormField(
+                        controller: _email,
+                        decoration: CommonStyle.textFieldStyle(
+                          labelText:'E-mail', 
+                          hintText: 'Insira o e-mail'
                         ),
-                    ),
-                    label: const Text('Continue with Google',
-                      style: TextStyle(fontSize: 18),
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Informe o e-mail corretamente!';
+                          }
+                          return null;
+                        },
                       ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Pallete.orangeColor,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)
-                        )
+                    ),
+                    Padding(
+                      padding: formFieldPadding,
+                      child: TextFormField(
+                        controller: _password,
+                        obscureText: true,
+                        decoration: CommonStyle.textFieldStyle(
+                          labelText:'Senha', 
+                          hintText: 'Insira sua senha'
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Informa sua senha!';
+                          } else if (value.length < 6) {
+                            return 'Sua senha deve ter no mínimo 6 caracteres';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: buttonPadding,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            signInWithEmailAndPassword(context);
+                          }
+                        },
+                        style: CommonStyle.buttonStyle(),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              Padding(
+                padding: buttonPadding,  
+                child: ElevatedButton.icon(
+                  onPressed: () => signInWithGoogle(context, ref),
+                  style: CommonStyle.buttonStyle(),
+                  icon: CircleAvatar(
+                    child: Image.asset(
+                      Constants.googleLogoPath,
+                      width: 35,
+                    ),
+                  ),
+                  label: const Text('Continue with Google',
+                    style: TextStyle(fontSize: 18),
+                    ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextButton(
+                  onPressed: () {
+                    navigateToSignUpScreen(context);
+                  },
+                  child: const Text('Não tem conta? Cadastre-se aqui!',
+                    style: TextStyle(
+                      fontSize: 18
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.popAndPushNamed(context, SignUpScreen.id);
-                    },
-                    child: const Text('Não tem conta? Cadastre-se aqui!',
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
