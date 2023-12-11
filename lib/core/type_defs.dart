d836c88148e7643bc4445ec42c788551e056b110
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-10 23:46:37 -0300

    Corrected a mistake

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -2,4 +2,4 @@ import 'package:fpdart/fpdart.dart';
 import 'package:joga_junto/core/failure.dart';
 
 typedef FutureEither<T> = Future<Either<Failure, T>>;
-typedef FutureVoid = Future<void>;
+typedef FutureVoid = FutureEither<void>;
