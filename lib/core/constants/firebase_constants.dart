3c1f452c279883af0f6485c0dc49dfa5925f7369
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-10 23:49:43 -0300

    added statisticsColleciton

--- lib/core/constants/firebase_constants.dart
+++ lib/core/constants/firebase_constants.dart
@@ -1,6 +1,7 @@
 class FirebaseConstants {
   static const usersCollection = 'users';
   static const teamsCollection = 'teams';
+  static const statisticsCollection = 'statistics';
   static const postsCollection = 'posts';
   static const commentsCollection = 'comments';
 }
