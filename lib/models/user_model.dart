cd17036634f08f99f8f1d3be089c75cfb0cc32a8
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-12-10 23:49:16 -0300

    added uid field back

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,19 +1,25 @@
+
 class UserModel {
+  final String uid;
   final String profilePic;
   final String email;
   final String password;
   UserModel({
+    required this.uid,
     required this.profilePic,
     required this.email,
     required this.password,
   });
+  
 
   UserModel copyWith({
+    String? uid,
     String? profilePic,
     String? email,
     String? password,
   }) {
     return UserModel(
+      uid: uid ?? this.uid,
       profilePic: profilePic ?? this.profilePic,
       email: email ?? this.email,
       password: password ?? this.password,
@@ -22,6 +28,7 @@ class UserModel {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'uid': uid,
       'profilePic': profilePic,
       'email': email,
       'password': password,
@@ -30,6 +37,7 @@ class UserModel {
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
+      uid: map['uid'] as String,
       profilePic: map['profilePic'] as String,
       email: map['email'] as String,
       password: map['password'] as String,
@@ -37,18 +45,26 @@ class UserModel {
   }
 
   @override
-  String toString() => 'UserModel(profilePic: $profilePic, email: $email, password: $password)';
+  String toString() {
+    return 'UserModel(uid: $uid, profilePic: $profilePic, email: $email, password: $password)';
+  }
 
   @override
   bool operator ==(covariant UserModel other) {
     if (identical(this, other)) return true;
   
     return 
+      other.uid == uid &&
       other.profilePic == profilePic &&
       other.email == email &&
       other.password == password;
   }
 
   @override
-  int get hashCode => profilePic.hashCode ^ email.hashCode ^ password.hashCode;
+  int get hashCode {
+    return uid.hashCode ^
+      profilePic.hashCode ^
+      email.hashCode ^
+      password.hashCode;
+  }
 }
